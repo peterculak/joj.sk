2a4e7e3db7d95fc44c23185e6d9e3c43899a9a23
Peter Culak <peter@demobox.co>
2016-03-18 00:29:33 +0000

    debug

--- src/app/_shared/controllers/HomeCtrl.js
+++ src/app/_shared/controllers/HomeCtrl.js
@@ -14,6 +14,7 @@ angular.module('joj.shared')
     ctrl.isPlaying = false;
 
     $scope.selectedIndex = 0;
+    alert(mobileAndTabletcheck());
     if (mobileAndTabletcheck()) {
       $scope.selectedIndex = 1;
     }
