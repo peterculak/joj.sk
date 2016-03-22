7ee4f0836a180368d715da79b0beebd0fd034e5e
Peter Culak <peter@demobox.co>
2016-03-22 00:52:35 +0000

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 git pull;
 rm -rf dist;
-gulp build;
+mkdir dist;
 mkdir dist/bower_components;
 cp -r bower_components/videojs dist/bower_components/;
 cp -R bower_components/videojs/font dist/styles/;
+gulp build;
