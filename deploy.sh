6d5f091ab0da53285fe2b3be4abbaa1dcc5c0ff9
Peter Culak <peter@demobox.co>
2016-03-23 23:16:25 +0000

    Font deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ mkdir dist;
 mkdir dist/bower_components;
 cp -r bower_components/videojs dist/bower_components/;
 cp -R bower_components/videojs/font dist/styles/;
+mkdir dist/styles/font;
+cp dist/styles/vjs.* dist/styles/font
 gulp build;
