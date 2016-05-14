5e751e75e802450d996166f8d62a566068a54560
Peter Culak <peter@demobox.co>
2016-05-14 18:09:59 +0200

    Disable nav and comments

--- web/wp-content/themes/materialwp/single.php
+++ web/wp-content/themes/materialwp/single.php
@@ -17,13 +17,15 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', 'single' ); ?>
 
-				<?php materialwp_post_nav(); ?>
+				<?php
+//					materialwp_post_nav();
+				?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+//					if ( comments_open() || get_comments_number() ) :
+//						comments_template();
+//					endif;
 				?>
 
 			<?php endwhile; // end of the loop. ?>
