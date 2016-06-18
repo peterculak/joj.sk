bda02f17f5b49970254a4f2ab908d5793f173b1a
Peter Culak <peter@demobox.co>
2016-06-18 14:30:19 +0200

    Trying to fix GA

--- web/wp-content/themes/materialwp/header.php
+++ web/wp-content/themes/materialwp/header.php
@@ -40,6 +40,16 @@
 <!--<script src="scripts/app-ecc14a0b.js"></script>-->
 <?php require_once(__DIR__ . '/../../../app.html'); ?>
 
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-74974547-1', 'streamtv.sk');
+	ga('set', 'anonymizeIp', false);
+	ga('send', 'pageview');
+</script>
 </head>
 
 <body layout="column" ui-view <?php body_class(); ?> ng-controller="HomeCtrl as ctrl">
